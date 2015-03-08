76620081bb3079232033d407473f18184e1875df
Justin Ward <jward@modernmagnetic.com>
2015-03-08 23:04:36 +1100

    wrote all the code but cannot get tests or code to run. Can get node working in rubymine but not this app tried some hacks to get it working but my guess is it runs in a browser, not a terminal, therefore I do not know enough to get it to run.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -2,6 +2,8 @@
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
 'use strict';
+console.log("Debug -showing that NodeJS runs this file")
+playToFive();
 
 function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
@@ -21,18 +23,27 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
+// Write an expression that operates on a variable called `move`
+// If a `move` has a value, your expression should evaluate to that value.
+// However, if `move` is not specified / is null, your expression should equal `getInput()`.
+// JLW -can probably use ternary operator for one liner, but need to practise JS syntax.
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    if (move != null) {
+        return move;
+    } else {
+        return getInput();
+    }
 }
-
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    // JLW -can probably use ternary operator for one liner, but need to practise JS syntax.
+    if (move != null) {
+        return move;
+    } else {
+        return randomPlay();
+    }
 }
 
 function getWinner(playerMove,computerMove) {
@@ -40,7 +51,21 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove == computerMove) {
+        winner ="tie"
+    } else if(playerMove == "rock" && computerMove == "paper"){
+        winner = "player"
+    } else if(playerMove == "rock" && computerMove == "scissors") {
+        winner = "computer"
+    } else if(playerMove == "paper" && computerMove == "scissors") {
+        winner = "computer"
+    } else if(playerMove == "paper" && computerMove == "rock") {
+        winner = "player"
+    } else if(playerMove == "scissors" && computerMove == "paper") {
+        winner = "player"
+    }  else if(playerMove == "scissors" && computerMove == "rock") {
+        winner = "computer"
+    }
     return winner;
 }
 
@@ -50,6 +75,23 @@ function playToFive() {
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
+    var playerMove;
+    var computerMove;
+    var winner;
+
+    while (playerWins < 5 || computerWins < 5){
+        playerMove = getPlayerMove();
+        computerMove = getComputerMove();
+        console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
+
+        winner = getWinner(playerMove, computerMove)
+        if (winner == "computer"){
+            computerWins += 1;
+        } else if (winner == "player"){
+            playerWins += 1;
+        }
+        console.log("The score is currently " + playerWins + " to " + computerWins + "\n");
+    }
     return [playerWins, computerWins];
 }
 
